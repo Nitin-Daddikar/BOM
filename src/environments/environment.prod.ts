fa174792436d9c3e5f0453fdbd6c9e6bcd5c09b2
Nitin Daddikar <clarus.nitin@gmail.com>
2025-11-05 11:33:39 +0530

    keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,6 +6,6 @@ export const environment = {
   apiUrl: 'https://newapi.swastikarts.com/api',
   useDummayApi: true,
   dummyApiUrl: 'https://api.jsonbin.io/v3/b',
-  masterKey : '',
-  accessKey: ''
+  masterKey : '$2a$10$0m41yridpI82BbvjLJHsIuLb7ynKKA56BYFAMNZmLKQEpz6Z.2.xu',
+  accessKey: '$2a$10$zkmj6cy9fsWEasGMgYpUcOWLjqnbe1113J907YeoKWuFecb4DObUm'
 };
